31b168bb47a870487f4a4580c2a170d37c0b4033
Mattheus Toscano <mattheustoscano@outlook.com>
2019-03-08 21:45:37 -0300

    Definindo Controlador do Usuário com o primeiro método de cadastro.

---
+++ oficinasolution/ProjetoOficina.Services/Models/Perfil/PerfilUsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoOficina.Services.Models.Perfil
+{
+    public class PerfilUsuarioModel
+    {
+        public int IdPerfil { get; set; }
+        public string Nome { get; set; }
+    }
+}
