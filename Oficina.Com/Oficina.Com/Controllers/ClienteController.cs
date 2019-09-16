a4cbaab09d2e2edfe818b7efcfd17080662f8b2d
bgvinicius <bgvinicius@hotmail.com>
2019-09-15 22:35:31 -0300

    Criação da tela de clientes.

---
+++ Oficina.Com/Oficina.Com/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oficina.Com.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+        
+
+    }
+}
