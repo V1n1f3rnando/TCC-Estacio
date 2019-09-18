88592f891d27fbb6e1c837504b305c764534c8c2
bgvinicius <bgvinicius@hotmail.com>
2019-09-18 19:55:38 -0300

    Alterações

--- Oficina.Com/Oficina.com.Dados/Contextos/Context.cs
+++ Oficina.Com/Oficina.com.Dados/Contextos/Context.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.Entity;
+using Oficina.Com.Entidades;
 
 namespace Oficina.com.Dados.Contextos
 {
@@ -16,6 +18,12 @@ namespace Oficina.com.Dados.Contextos
 
         }
 
-        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //adicionar as classes de mapeamento..
+            //modelBuilder.Configurations.Add(new );
+        }
+
+        public DbSet<Cliente> Cliente { get; set; } //LAMBDA..
     }
 }
