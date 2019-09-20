db07b47e5955a227eb8e7a8f42fb251bcd74ab44
bgvinicius <bgvinicius@hotmail.com>
2019-09-19 21:06:29 -0300

    Alterações.

--- Oficina.Com/Oficina.Com/Controllers/ClienteController.cs
+++ Oficina.Com/Oficina.Com/Controllers/ClienteController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Oficina.Com.Entidades;
+using Oficina.Com.Models;
+using OFicina.Com.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +14,42 @@ namespace Oficina.Com.Controllers
         // GET: Cliente
         public ActionResult Consulta()
         {
+            List<ClienteViewModel> lista = new List<ClienteViewModel>();
+
+            try
+            {
+                ClienteNegocio clienteNegocio = new ClienteNegocio();
+                foreach (var item in clienteNegocio.Consulta())
+                {
+                    ClienteViewModel model = new ClienteViewModel();
+
+                    model.Id = item.Id;
+                    model.Nome = item.Nome;
+                    model.Telefone = item.Telefone;
+                    model.Cpf = item.Cpf;
+                    model.DataNascimento = item.DataNascimento;
+                    model.Email = item.Email;
+                    model.Endereco = item.Endereco;
+                    model.Veiculos = item.Veiculos;
+
+                    lista.Add(model);
+                }
+
+                return View(lista);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        
+        }
+
+        public ActionResult Cadastro()
+        {
+
+
             return View();
         }
         
