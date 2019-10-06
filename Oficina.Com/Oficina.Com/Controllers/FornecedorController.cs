8a81c086ef8f082516b8fe464fb884a2767f09cc
bgvinicius <bgvinicius@hotmail.com>
2019-10-06 11:32:19 -0300

    sadsad

---
+++ Oficina.Com/Oficina.Com/Controllers/FornecedorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oficina.Com.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        // GET: Fornecedor
+        public ActionResult ConsultarFornecedor()
+        {
+            return View();
+        }
+    }
+}
---
+++ Oficina.Com/Oficina.Com/Controllers/OrdemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oficina.Com.Controllers
+{
+    public class OrdemController : Controller
+    {
+        // GET: Ordem
+        public ActionResult ConsultarOrdem()
+        {
+            return View();
+        }
+    }
+}
