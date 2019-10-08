a1e9214f8f85c46f9062a0b03f94d5a95c9ecc09
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2019-10-08 13:27:15 -0300

    Alterações.

--- Oficina.Com/Oficina.Com/Controllers/VeiculoController.cs
+++ Oficina.Com/Oficina.Com/Controllers/VeiculoController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Oficina.Com.Entidades;
+using Oficina.Com.Models;
+using OFicina.Com.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,36 @@ namespace Oficina.Com.Controllers
         // GET: Veiculo
         public ActionResult ConsultarVeiculo()
         {
-            return View();
+            try
+            {
+                List<VeiculoViewModel> lstVeiculos = new List<VeiculoViewModel>();
+                VeiculoNegocio veiculoNegocio = new VeiculoNegocio();
+
+                foreach (Veiculo veiculo in veiculoNegocio.Consulta())
+                {
+                    VeiculoViewModel model = new VeiculoViewModel();
+                    model.Id = veiculo.Id;
+                    model.Placa = veiculo.Placa;
+                    model.Modelo = veiculo.Modelo;
+                    model.Motor = veiculo.Motor;
+                    model.Obs = veiculo.Obs;
+                    model.Tipo = veiculo.Tipo;
+                    model.Ano = veiculo.Ano;
+                    model.ClienteId = veiculo.ClienteId;
+                    model.Cor = veiculo.Cor;
+
+                    lstVeiculos.Add(model);
+                }
+
+                return View(lstVeiculos);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+
         }
     }
 }
---
+++ Oficina.Com/Oficina.Com/Models/VeiculoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Oficina.Com.Entidades.Tipos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oficina.Com.Models
+{
+    public class VeiculoViewModel
+    {
+        public int Id { get; set; }
+        public string Placa { get; set; }
+        public string Ano { get; set; }
+        public TipoVeiculo Tipo { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string Motor { get; set; }
+        public string Obs { get; set; }
+
+        public int ClienteId { get; set; }
+    }
+}
