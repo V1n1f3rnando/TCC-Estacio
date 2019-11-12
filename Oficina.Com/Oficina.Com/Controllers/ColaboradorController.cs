2b932d144fd0d559157e1d5052881100d4cc1c30
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2019-11-12 08:05:14 -0300

    Ultimas att !!

---
+++ Oficina.Com/Oficina.Com/Controllers/ColaboradorController.cs
@@ -0,0 +1,43 @@
+﻿using Oficina.Com.Entidades;
+using Oficina.Com.Models;
+using OFicina.Com.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oficina.Com.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        // GET: Colaborador
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult Consulta()
+        {
+            ColaboradorViewModel model = new ColaboradorViewModel();
+            ColaboradorNegocio colaboradorNegocio = new ColaboradorNegocio();
+            List<Colaborador> lstColaborador = colaboradorNegocio.Consulta();
+            List<ColaboradorViewModel> lstModel = new List<ColaboradorViewModel>();
+
+            foreach (var c in lstColaborador)
+            {
+                model.Id = c.Id;
+                model.Nome = c.Nome;
+                model.Salario = c.Salario;
+                model.Telefone = c.Telefone;
+                model.Cargo = c.Cargo;
+                model.Email = c.Email;
+                model.EnderecoId = c.EnderecoId;
+
+                lstModel.Add(model);
+            }
+
+            return View(lstModel);
+
+        }
+    }
+}
---
+++ Oficina.Com/Oficina.Com/Models/ColaboradorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Oficina.Com.Entidades.Tipos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oficina.Com.Models
+{
+    public class ColaboradorViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public Cargo Cargo { get; set; }
+        public decimal Salario { get; set; }
+        public int EnderecoId { get; set; }
+    }
+}
